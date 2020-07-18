889fbca536392bcd15d71af93742e3255aea7544
Matthew Meade <mafumeade@gmail.com>
2020-07-18 14:31:15 -0230

    Center image in bio

--- src/components/Bio/Bio.jsx
+++ src/components/Bio/Bio.jsx
@@ -1,48 +1,50 @@
-import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
-import './styles.scss';
-import useWindowDimensions from '../../hooks/windowDims';
+import "./styles.scss";
+import useWindowDimensions from "../../hooks/windowDims";
 
 const BIO_TEXT = [
-    'A full stack JavaScript developer who hates writing bios about himself',
-    'Scroll down to see some of my featured projects'
+  "A full stack JavaScript developer who hates writing bios about himself",
+  "Scroll down to see some of my featured projects",
 ].map((t, n) => <p key={n}>{t}</p>);
 
 const LINKS = require("../../data/generalLinks.json").map(({ icon, text, href }) => (
-    <div className='linkDiv' key={href}>
-        <a href={href} className='text-white' target='_blank' rel='noopener noreferrer'>
-            <i className={`${icon} text-primary`} /> {text}
-        </a>
-    </div>
+  <div className="linkDiv" key={href}>
+    <a href={href} className="text-white" target="_blank" rel="noopener noreferrer">
+      <i className={`${icon} text-primary`} /> {text}
+    </a>
+  </div>
 ));
 
 export default function Bio() {
-    const { breakpoint } = useWindowDimensions();
-    const isSmall = breakpoint <= 1;
-    const sizeClass = isSmall ? 'small' : 'large';
+  const { breakpoint } = useWindowDimensions();
+  const isSmall = breakpoint <= 1;
+  const sizeClass = isSmall ? "small" : "large";
 
-    const nameClasses = isSmall ? `text-white text-center` : `text-primary`;
-    const NAME = <h1 className={nameClasses}>Matthew Meade</h1>;
+  const nameClasses = isSmall ? `text-white text-center` : `text-primary`;
+  const NAME = <h1 className={nameClasses}>Matthew Meade</h1>;
 
-    return (
-        <Container className={`bio ${sizeClass}`}>
-            <Row>
-                <Col md={4}>
-                    {isSmall && NAME}
+  return (
+    <Container className={`bio ${sizeClass}`}>
+      <Row>
+        <Col md={4}>
+          {isSmall && NAME}
 
-                    <img alt='' src='/prof_pic_trans_cropped.png' />
-                </Col>
-                <Col md={8} sm={12} className='text-white'>
-                    <div className='bio_text'>
-                        {!isSmall && NAME}
+          <div className="imgContainer">
+            <img alt="" src="/prof_pic_trans_cropped.png" />
+          </div>
+        </Col>
+        <Col md={8} sm={12} className="text-white">
+          <div className="bio_text">
+            {!isSmall && NAME}
 
-                        {BIO_TEXT}
+            {BIO_TEXT}
 
-                        {LINKS}
-                    </div>
-                </Col>
-            </Row>
-        </Container>
-    );
+            {LINKS}
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
