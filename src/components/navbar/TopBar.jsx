34d0781c0862b4fbf792e4b0f741952ec430986b
Matthew Meade <matthew.meade@radient360.com>
2020-08-04 21:42:55 -0230

    Simplify navbar and other CSS tweaks

--- src/components/navbar/TopBar.jsx
+++ src/components/navbar/TopBar.jsx
@@ -1,48 +1,24 @@
-import React, { Component } from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import "./styles.scss";
-import { coverLinks } from "../../data/portfolioData";
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import './styles.scss';
+import { coverLinks } from '../../data/portfolioData';
 
-const HOVER_TIMEOUT_MS = 1500;
-
-export default class TopBar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { hoveredLink: "" };
-    this.timeOut = 0;
-  }
-
-  render() {
-    const { breakpoint } = this.props;
-    const { hoveredLink } = this.state;
-
+export default function TopBar({ breakpoint }) {
     const linkRows = coverLinks.map(({ icon, text, shortText, href }) => {
-      const showTextClass = hoveredLink === href || breakpoint < 4 ? "showText" : "";
-      return (
-        <Col
-          key={href}
-          onMouseEnter={() => {
-            clearTimeout(this.timeOut);
-            this.setState({ hoveredLink: href });
-          }}
-          onMouseLeave={() => {
-            this.timeOut = setTimeout(() => {
-              this.setState({ hoveredLink: "" });
-            }, HOVER_TIMEOUT_MS);
-          }}
-          className={`barLink ${showTextClass}`}
-        >
-          <a href={href} target="_blank" rel="noopener noreferrer" alt={text} relrel="noopener noreferrer">
-            <i className={`${icon} icon`} />{" "}
-            {breakpoint > 0 && <span className="text">{breakpoint > 3 ? text : shortText || text}</span>}
-          </a>
-        </Col>
-      );
+        return (
+            <Col key={href} className={`barLink`}>
+                <a href={href} target='_blank' rel='noopener noreferrer' alt={text}>
+                    <i className={`${icon} icon`} />{' '}
+                    {breakpoint > 0 && (
+                        <span className='text'>{breakpoint > 3 ? text : shortText || text}</span>
+                    )}
+                </a>
+            </Col>
+        );
     });
     return (
-      <Container className="topBar" fluid>
-        <Row>{linkRows}</Row>
-      </Container>
+        <Container className='topBar' fluid>
+            <Row>{linkRows}</Row>
+        </Container>
     );
-  }
 }
